74a931277f6b8e04175fc77a560866ab6601ab4f
Andrés F. Patiño <andresfelipepatino5@gmail.com>
2019-03-13 17:39:40 -0500

    Estilos Landing

--- public/landing.php
+++ public/landing.php
@@ -29,6 +29,7 @@
                 <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
                 <title>Landing Page</title>  
                 <?php $PATH = plugin_dir_url( __FILE__ );  ?>
+                <link href="https://fonts.googleapis.com/css?family=Poppins:700|Raleway:300" rel="stylesheet">
                 <link rel="stylesheet" href="<?php echo $PATH . 'css/bootstrap.css'; ?>"> 
                 <link rel="stylesheet" href="<?php echo $PATH . 'css/main.css'; ?>">        
             </head>
@@ -44,63 +45,72 @@
                     </div>
                 </div>
             </header>
-            <div class="principal pt-100 pb-100 ">
+            <div class="principal pt-100">
                 <main class="container">  
                     <div class="row align-items-center justify-content-between">
-                        <div class="col-lg-4 col-md-4">  
-                           <img class="img-fluid" src="http://placehold.it/600" alt="">
+                        <div class="col-lg-5 col-md-5">  
+                           <img class="img-fluid" src="<?php echo $PATH ?>img/mockup.png" alt="">
                         </div>
 
-                        <div class="col-lg-8 col-md-8">                      
-                            <h1><?php echo esc_html(get_option('titulo')); ?></h1>
+                        <div class="col-lg-7 col-md-7">   
+                            <div class="info">
+                            <h1 class="title"><?php echo esc_html(get_option('titulo')); ?></h1>
                             <p><?php echo esc_html(get_option('intro_txt')); ?></p>           
-                            <form class="contacto" method="POST">
-
-                                <div class="form-row">
-                                    <div class="col">
-                                        <input class="form-control" type="text" name="nombre" placeholder="Nombres" required>
+                                <form class="contacto" method="POST">
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <input class="form-control" type="text" name="nombre" placeholder="Nombres" required>
+                                        </div>
+                                        <div class="col">
+                                            <input class="form-control" type="text" name="apellido" placeholder="Apellidos" required>                        
+                                        </div>
                                     </div>
-                                    <div class="col">
-                                        <input class="form-control" type="text" name="apellido" placeholder="Apellidos" required>                        
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <input class="form-control" type="emai" name="correo" placeholder="Correo" required>
+                                        </div>
+                                        <div class="col">
+                                            <input class="form-control" type="tel" name="telefono" placeholder="Teléfono" required>
+                                        </div>
                                     </div>
-                                </div>
-
-                                <div class="form-row">
-                                    <div class="col">
-                                        <input class="form-control" type="emai" name="correo" placeholder="Correo" required>
+                                    <div class="campo">  
+                                        <?php $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"); ?>
+                                        <input type="hidden" name="fecha" value="<?php echo date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ; ?>">
+                                    </div>   
+                                    <div class="form-row">  
+                                        <div class="col">                               
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" class="custom-control-input" id="politicas" required checked>
+                                                <label class="custom-control-label" for="politicas"> Acepto las <a target="_blank" href="<?php echo esc_html(get_option('link_politicas')); ?>">políticas de privacidad</a> </label>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <input type="submit" name="enviar" class="button" value="Solicitar información">
+                                        </div>
                                     </div>
-                                    <div class="col">
-                                        <input class="form-control" type="tel" name="telefono" placeholder="Teléfono" required>
-                                    </div>
-                                </div>
-
-                                <div class="campo">  
-                                    <?php $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"); ?>
-                                    <input type="hidden" name="fecha" value="<?php echo date('d')." de ".$meses[date('n')-1]. " del ".date('Y') ; ?>">
-                                    
-                                </div>
-                                
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="politicas" required checked>
-                                    <label class="custom-control-label" for="politicas"> Acepto las <a target="_blank" href="<?php echo esc_html(get_option('link_politicas')); ?>">políticas de privacidad</a> </label>
-                                </div>
-
-                                <input type="submit" name="enviar" class="button" value="Solicitar información">
-                                <input type="hidden" name="oculto" value="1"> 
-
-                                <!-- Mensaje gracias-->
-                                <?php if (isset($_POST['enviar']) && $_POST['oculto'] == "1"){ 
-                                    $nombre = sanitize_text_field( $_POST['nombre']);
-                                    $mensaje = esc_html(get_option('thanks_page')); ?>
-                                    <div class="alert alert-success fade show" role="alert">
-                                      <strong><?php echo $nombre . ', '?></strong> <?php echo $mensaje ?>
-                                    </div>
-                                <?php }  ?>
-
-                            </form>
+                                    <input type="hidden" name="oculto" value="1"> 
+                                    <!-- Mensaje gracias-->
+                                    <?php if (isset($_POST['enviar']) && $_POST['oculto'] == "1"){ 
+                                        $nombre = sanitize_text_field( $_POST['nombre']);
+                                        $mensaje = esc_html(get_option('thanks_page')); ?>
+                                        <div class="form-row">
+                                            <div class="alert alert-success fade show" role="alert">
+                                              <strong><?php echo $nombre . ', '?></strong> <?php echo $mensaje ?>
+                                            </div>
+                                        </div>
+                                    <?php }  ?>
+                                </form>
+                            </div>                   
                         </div>  
                     </div>
                 </main>
+                <footer>
+                    <div class="container">
+                        <div class="row align-items-center justify-content-center">
+                            <p>Todos los derechos reservados &reg; <?php echo date("Y"); ?>, <a href="http://felipepatino.com/" target="_blank">Andrés Felipe Patiño</a> </p>
+                        </div>
+                    </div>
+                </footer>
             </div>
                 
         </body>
